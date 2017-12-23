f44f44ffb73a13d5d2d2922c0426cc3ccb75699e
Rob Halff <rob.halff@gmail.com>
2017-12-23 16:14:10 +0000

    clean

--- keystone.js
+++ keystone.js
@@ -2,32 +2,13 @@ require('dotenv').config();
 const keystone = require('keystone')
 const favicon = require('express-favicon')
 const express = require('express')
-//const handlebars = require('handlebars')
-//const mongoose = require('mongoose')
-//const app = express()
 
 keystone.init({
   'name': 'RobbertHalff',
   'brand': 'RobbertHalff',
-
   'less': 'public',
   'static': 'public',
   'favicon': 'public/favicon.ico',
-  /*
-  'views': 'templates/views',
-  'view engine': 'hbs',
-
-  'custom engine': handlebars.create({
-    layoutsDir: 'templates/views/layouts',
-    partialsDir: 'templates/views/partials',
-    defaultLayout: 'default',
-    helpers: new require('./templates/views/helpers')(),
-    extname: '.hbs',
-  }).engine,
-
-  'emails': 'templates/emails',
-  */
-
   'auto update': true,
   'session': true,
   'auth': true,
@@ -44,25 +25,6 @@ keystone.set('locals', {
   utils: keystone.utils,
   editable: keystone.content.editable
 })
-
-/*
-keystone.set('email locals', {
-  logo_src: '/images/logo-email.gif',
-  logo_width: 194,
-  logo_height: 76,
-  theme: {
-    email_bg: '#f9f9f9',
-    link_color: '#2697de',
-    buttons: {
-      color: '#fff',
-      background_color: '#2697de',
-      border_color: '#1a7cb7',
-    },
-  },
-})
-
-keystone.set('email tests', require('./routes/emails'))
-*/
 
 keystone.set('cors allow origin', true);
 keystone.set('routes', require('./routes'))
